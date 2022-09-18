fbb174aa2b090d47c8f57b876809f160b88debb0
Murali Govardhana <murali.girikg@gmail.com>
2022-09-18 17:13:19 +0530

    custom keys

--- keyboards/handwired/tractyl_manuform/5x6_right/keymaps/default/keymap.c
+++ keyboards/handwired/tractyl_manuform/5x6_right/keymaps/default/keymap.c
@@ -41,11 +41,11 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_5x6_right(
-     KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-     KC_ESC, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-                      KC_LBRC, LOWER,                                                       LOWER, KC_EQL,
+     KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
+     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_EQL,
+     KC_ESC, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_PGUP,
+     KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_PGDN,
+                      KC_LWIN, LOWER,                                                       KC_RSFT, KC_QUOT,
                                       KC_LCTL , RAISE,                                    KC_SPACE,
                                       KC_LALT,KC_BSPC,                                  KC_ENTER,
                                       DRG_TOG, KC_MS_BTN2,                         KC_MS_BTN1, DRG_TOG
@@ -53,9 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT_5x6_right(
      KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-     _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
+     _______,_______,_______,_______,_______,KC_LBRC,                        KC_LEFT_BRACKET, KC_RIGHT_BRACKET , KC_PIPE , KC_P9 ,_______,KC_PLUS,
      _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_LEFT, KC_DOWN , KC_UP , KC_RIGHT ,KC_MINS,KC_PIPE,
-     _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+     _______,_______,_______,_______,_______,_______,                        KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
                      _______,KC_PSCR,                                                        _______, KC_P0,
                                              _______,_______,                _______,
                                              _______,_______,                _______,
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                _______,_______,                _______,
                                                _______,_______,                _______,
                                                _______,_______,         _______,_______
-  ),
+  )
 };
 
 // void keyboard_post_init_user(void) {
